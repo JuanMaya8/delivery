73d715b781eaeb52f7589584e454199a292a9a32
JuanMaya8 <juanmaya@campusucc.edu.co>
2026-04-08 18:41:53 -0500

    Actualizar cambios locales antes de push

--- lib/core/constants/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primaryOrange = Color(0xFFFF7043); // Naranja del diseño
-  static const Color backgroundGrey = Color(0xF9F9F9);
-  static const Color textBlack = Color(0xFF2D2D2D);
-  static const Color textGrey = Color(0xFF9E9E9E);
-}
